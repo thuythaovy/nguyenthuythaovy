a1128d8bab0b24372a1d94b4f6d1e05bbf2344ae
thuythaovy <nguyenthuythaovy3105@gmail.com>
2024-08-18 11:42:37 +0700

    Add files via upload

---
+++ baitapnamnhuan.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 11:38:51 2024
+
+@author: nguyenthuythaovy
+"""
+
+a=int(input("Nhap so nam:"))
+if (a%4==0) and (a%100!=0):
+    print("Nam",a,"la nam nhuan")
+else:
+    print("Nam",a,"khong la nam nhuan")
+
+      
